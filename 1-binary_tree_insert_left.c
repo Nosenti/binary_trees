fddcdfb8b481d9a61ec6537dc9f958c06b3f1e5e
Nosenti <inno100os@gmail.com>
2024-04-09 18:56:13 +0200

    making the new node parent of old node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -34,6 +34,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = old_node;
 		parent->left = new_node;
 		old_node->left = NULL;
+		old_node->parent = new_node;
 	}
 	else
 	{
