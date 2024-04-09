b900bbfff6bfb68c34143b77eeab1f126ef42ffe
Nosenti <inno100os@gmail.com>
2024-04-09 22:20:32 +0200

    correction on checking left and right nodes

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,12 +8,13 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left || node->right)
-	{
-		return (1);
-	}
-	else
+	if (!node)
 	{
 		return (0);
 	}
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
 }
