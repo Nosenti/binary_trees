cea12fb3c446a7d42f89f6329bbdc6d80cfc6187
Nosenti <inno100os@gmail.com>
2024-04-09 23:14:54 +0200

    nodes that are not leaves

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - measure nodes of a binary tree
+ * @tree: the provided tree
+ *
+ * Return: size_t nodes of the tree
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+	size_t left_tree = 0;
+	size_t right_tree = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (tree->left || tree->right)
+	{
+		return (1);
+	}
+
+	left_tree = binary_tree_nodes(tree->left);
+	right_tree = binary_tree_nodes(tree->right);
+
+	return (left_tree + right_tree);
+}
