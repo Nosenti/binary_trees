30b07c4911e33a6b5417dcae0bdd756d6261d4f4
Nosenti <inno100os@gmail.com>
2024-04-09 22:08:28 +0200

    check if the node is the leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check if a node is a leaf
+ * @node: node to check whether it is a leaf or node
+ * Return: 1 if leaf and 0 otherwise
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node && (node->left || node->right))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
