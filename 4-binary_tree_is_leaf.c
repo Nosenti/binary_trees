509efa8092ea728620d130cc0722ef8a793b0ca5
Nosenti <inno100os@gmail.com>
2024-04-09 22:14:12 +0200

    using or operands for left and right nodes

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,12 +8,12 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node || !node->left || !node->right)
+	if (node->left || node->right)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
