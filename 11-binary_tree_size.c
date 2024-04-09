2365bc301700c84b503fa9e55fc4f870cf9ccc8d
Nosenti <inno100os@gmail.com>
2024-04-09 23:00:09 +0200

    size of the tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measure size of a binary tree
+ * @tree: the provided tree
+ *
+ * Return: size_t size of the tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+
+	size_t left_tree, right_tree;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (!tree->left && !tree->right)
+	{
+		return (0);
+	}
+
+	left_tree = binary_tree_size(tree->left) + 1;
+	right_tree = binary_tree_size(tree->right);
+
+	return (left_tree + right_tree);
+}
