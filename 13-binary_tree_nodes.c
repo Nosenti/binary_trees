dad33400e567f850966676859357c44b91babf94
Nosenti <inno100os@gmail.com>
2024-04-09 23:16:04 +0200

    count root

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -23,7 +23,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (1);
 	}
 
-	left_tree = binary_tree_nodes(tree->left);
+	left_tree = binary_tree_nodes(tree->left) + 1;
 	right_tree = binary_tree_nodes(tree->right);
 
 	return (left_tree + right_tree);
